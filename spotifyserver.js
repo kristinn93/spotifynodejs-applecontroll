0a4a3b9c9d744adbdcaa21cbe76d273946d77b67
Kristinn Júlíusson <kristinn@Kristinns-Air.lan>
2015-08-03 22:25:22 +0000

    creating callback for all function and error handling

--- spotifyserver.js
+++ spotifyserver.js
@@ -9,61 +9,53 @@ const PORT=8001;
 var exec = require('child_process').exec;
 
 app.get('/play', function(req, res) {
-  console.log("Should play");
-  playMusic();
-  res.json('OK');
+	var play = './spotifyscripts/run spotifyscripts/play.scpt';
+	doSpot(play, function(err, stdout){
+		if(err) {
+			res.status(500).json('Can\'t play');
+		}
+		res.json('OK');
+	});
 });
 
 app.get('/pause', function(req, res) {
-  console.log("Should pause");
-  pauseMusic();
-  res.json('OK');
+	var pause = './spotifyscripts/run spotifyscripts/pause.scpt';
+	doSpot(pause, function(err, stdout) {
+		if(err) {
+			res.status(500).json('Can\'t pause');
+		}
+		res.json('OK');
+	});
 });
 
 app.get('/next', function(req, res) {
-  playNext();
-  res.json('OK');
+	var next = './spotifyscripts/run spotifyscripts/next.scpt';
+	doSpot(next, function(err, stdout){
+		if(err) {
+			res.status(500).json('Can\'t play next song');
+		}
+		res.json('OK');
+	});
 });
 
 app.get('/currSong', function(req, res) {
-  currentSong(function(err, songName) {
-    if (err) {
-      res.status(500).json('Could not find song name');
-    }
-    var artist = songName.split('/')[0];
-    var song = songName.split('/')[1];
-    res.json({artist: artist, song: song});
-  });
+	var current = './spotifyscripts/run spotifyscripts/currentSong.scpt';
+	doSpot(current, function(err, stdout) {
+		if(err) {
+			res.status(500).json('Can\'t find the name of song');
+		}
+		var artist = stdout.split('/')[0];
+		var song = stdout.split('/')[1];
+		res.json({artist: artist, song: song});
+	});
 });
 
-/**
- * ADD CALLBACKS TO ALL FUNCTIONS BELOW TO HANDLE ERRORS
- */
-function playMusic(){
-	var play = './spotifyscripts/run spotifyscripts/play.scpt';
-	exec(play, function(error, stdout, stderr) {
-  });
+function doSpot(command, cb){
+	exec(command, function(error, stdout, stderr) {
+    	cb(error, stdout);
+  	});
 };
 
-function pauseMusic(){
-	var pause = './spotifyscripts/run spotifyscripts/pause.scpt';
-	exec(pause, function(error, stdout, stderr) {
-  });
-};
-
-function playNext(){
-	var next = './spotifyscripts/run spotifyscripts/next.scpt';
-	exec(next, function(error, stdout, stderr) {
-  });
-};
-
-function currentSong(cb){
-	console.log("Getting current song");
-	var current = './spotifyscripts/run spotifyscripts/currentSong.scpt';
-	exec(current, function(error, stdout, stderr) {
-    cb(error, stdout);
-  });
-};
 
 //Create a server
 var server = http.createServer(app);
